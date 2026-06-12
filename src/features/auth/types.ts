884a90699a24bf5a8b531972dd4c6ce04084255f
Ryan Luong <lumintriet@gmail.com>
2026-06-12 21:28:12 +0700

    feat(auth/types): define SerializedUser interface.

--- src/features/auth/types.ts
+++ src/features/auth/types.ts
@@ -1,3 +1,12 @@
+import type { StrictOmit } from "@/types/utils";
 import type { auth } from "@/features/auth";
 
 export type User = typeof auth.$Infer.Session.user;
+
+export interface SerializedUser extends StrictOmit<
+  User,
+  "createdAt" | "updatedAt"
+> {
+  createdAt: string;
+  updatedAt: string;
+}
