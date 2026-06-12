51fa0698d745f5e7b6c2be6bad5a31c4a4aa311d
Ryan Luong <lumintriet@gmail.com>
2026-06-12 20:33:19 +0700

    feat(auth/errors): define UnauthenticatedError.

---
+++ src/features/auth/errors/unauthenticated.ts
@@ -0,0 +1,20 @@
+import { BaseError } from "@/errors/base";
+
+export class UnauthenticatedError extends BaseError<
+  "UNAUTHENTICATED_ERROR",
+  null
+> {
+  method: string;
+  url: string;
+
+  constructor({ method, url }: { method: string; url: string }) {
+    super({
+      name: "UnauthenticatedError",
+      message: `An unauthenticated request was made to "[${method}] ${url}".`,
+      code: "UNAUTHENTICATED_ERROR",
+      cause: null,
+    });
+    this.method = method;
+    this.url = url;
+  }
+}
