4321d68c1be48b862b1eb58bb0746ddf47669a75
Ryan Luong <lumintriet@gmail.com>
2026-06-13 11:46:22 +0700

    feat(error): define InternalServerError class.

--- src/error/constants.ts
+++ src/error/constants.ts
@@ -1,6 +1,7 @@
 export const UNEXPECTED_ERROR_CODE = "UNEXPECTED_ERROR";
 export const UNEXPECTED_ERROR_STATUS_CODE = 500;
 
-export const INTERNAL_SERVER_ERROR_CODE = "INTERNAL_SERVER_ERROR";
 export const INTERNAL_SERVER_ERROR_MESSAGE =
   "Something went wrong. Please try again later or contact support.";
+export const INTERNAL_SERVER_ERROR_CODE = "INTERNAL_SERVER_ERROR";
+export const INTERNAL_SERVER_ERROR_STATUS_CODE = 500;
