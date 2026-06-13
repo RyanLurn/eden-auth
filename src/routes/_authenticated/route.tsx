13a7f573adec082b04c827aedf4714d7b099434f
Ryan Luong <lumintriet@gmail.com>
2026-06-13 11:35:47 +0700

    feat(auth): define ErrorPage component for authenticated layout.

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,14 @@
-import { createFileRoute, redirect, Outlet } from "@tanstack/react-router";
+import {
+  type ErrorComponentProps,
+  createFileRoute,
+  useRouter,
+  redirect,
+  Outlet,
+} from "@tanstack/react-router";
+import { useState } from "react";
 
 import { getUserFn } from "@/features/auth/get-user-fn";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: async ({ location }) => {
@@ -19,9 +27,37 @@ export const Route = createFileRoute("/_authenticated")({
 
     return getUserResult.data;
   },
+  errorComponent: ErrorPage,
   component: AuthenticatedLayout,
 });
 
 function AuthenticatedLayout() {
   return <Outlet />;
+}
+
+function ErrorPage({ error }: ErrorComponentProps) {
+  const router = useRouter();
+  const [isRetrying, setIsRetrying] = useState(false);
+
+  async function retry() {
+    setIsRetrying(true);
+    await router.invalidate();
+    setIsRetrying(false);
+  }
+
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-y-3">
+      <h1 className="text-lg text-destructive">500 - Internal server error</h1>
+      <p>{error.message}</p>
+      <Button
+        onClick={() => {
+          void retry();
+        }}
+        disabled={isRetrying}
+        variant="destructive"
+      >
+        {isRetrying ? "Retrying..." : "Retry"}
+      </Button>
+    </div>
+  );
 }
