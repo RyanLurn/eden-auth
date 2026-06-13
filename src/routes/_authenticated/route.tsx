911bee3e79457e92948d919f761289f702f74c04
Ryan Luong <lumintriet@gmail.com>
2026-06-13 12:15:33 +0700

    feat(error): extract error pages into reusable components.

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,14 +1,9 @@
-import {
-  createFileRoute,
-  useRouter,
-  redirect,
-  Outlet,
-} from "@tanstack/react-router";
-import { useState } from "react";
+import { createFileRoute, redirect, Outlet } from "@tanstack/react-router";
 
+import { InternalServerErrorPage } from "@/components/error-pages/internal-server";
 import { InternalServerError } from "@/error/classes/internal-server";
+import { DefaultErrorPage } from "@/components/error-pages/default";
 import { getUserFn } from "@/features/auth/get-user-fn";
-import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: async ({ location }) => {
@@ -29,39 +24,13 @@ export const Route = createFileRoute("/_authenticated")({
   },
   errorComponent: ({ error }) => {
     if (error instanceof InternalServerError) {
-      return <ErrorPage error={error} />;
+      return <InternalServerErrorPage error={error} />;
     }
+    return <DefaultErrorPage error={error} />;
   },
   component: AuthenticatedLayout,
 });
 
 function AuthenticatedLayout() {
   return <Outlet />;
-}
-
-function ErrorPage({ error }: { error: InternalServerError }) {
-  const router = useRouter();
-  const [isRetrying, setIsRetrying] = useState(false);
-
-  async function retry() {
-    setIsRetrying(true);
-    await router.invalidate();
-    setIsRetrying(false);
-  }
-
-  return (
-    <div className="flex h-full flex-col items-center justify-center gap-y-3">
-      <h1 className="text-lg text-destructive">500 - Internal server error</h1>
-      <p>{error.message}</p>
-      <Button
-        onClick={() => {
-          void retry();
-        }}
-        disabled={isRetrying}
-        variant="destructive"
-      >
-        {isRetrying ? "Retrying..." : "Retry"}
-      </Button>
-    </div>
-  );
 }
