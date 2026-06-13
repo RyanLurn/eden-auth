ca40eb1bfd75808919f3dae065dbd12ddfbef6a4
Ryan Luong <lumintriet@gmail.com>
2026-06-13 09:04:38 +0700

    refactor(error): cleanup unused file.

--- src/error/serialized.ts
+++
@@ -1,8 +0,0 @@
-import type { INTERNAL_SERVER_ERROR_CODE } from "@/error/constants";
-import type { BaseError } from "@/error/classes/base";
-
-export type SerializedError = Pick<BaseError<string>, "message" | "code">;
-
-export interface SerializedInternalServerError extends SerializedError {
-  code: typeof INTERNAL_SERVER_ERROR_CODE;
-}
