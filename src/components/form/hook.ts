25124ef4956df8204c50122fbf016b084d64a24c
Ryan Luong <lumintriet@gmail.com>
2026-06-14 08:55:56 +0700

    feat(ui/form): define CheckField component and register it to the useAppForm hook.

--- src/components/form/hook.ts
+++ src/components/form/hook.ts
@@ -2,11 +2,12 @@ import { createFormHook } from "@tanstack/react-form";
 
 import { fieldContext, formContext } from "@/components/form/contexts";
 import { SubmitButton } from "@/components/form/submit-button";
+import { CheckField } from "@/components/form/check-field";
 import { TextField } from "@/components/form/text-field";
 
 export const { useAppForm } = createFormHook({
   fieldContext,
   formContext,
-  fieldComponents: { TextField },
+  fieldComponents: { TextField, CheckField },
   formComponents: { SubmitButton },
 });
