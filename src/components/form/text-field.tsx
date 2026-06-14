0fc72a5d1c70dbc6b7d28efa838d3a89e7d0aec1
Ryan Luong <lumintriet@gmail.com>
2026-06-14 08:57:49 +0700

    fix(ui/form): make TextField's label dynamic.

--- src/components/form/text-field.tsx
+++ src/components/form/text-field.tsx
@@ -6,18 +6,20 @@ import { FieldError, FieldLabel, Field } from "@/components/ui/field";
 import { useFieldContext } from "@/components/form/contexts";
 import { Input } from "@/components/ui/input";
 
-export function TextField(
-  textFieldProps: StrictOmit<
-    ComponentProps<typeof Input>,
-    "aria-invalid" | "onChange" | "onBlur" | "value" | "name" | "id"
-  >
-) {
+interface TextFieldProps extends StrictOmit<
+  ComponentProps<typeof Input>,
+  "aria-invalid" | "onChange" | "onBlur" | "value" | "name" | "id"
+> {
+  label: string;
+}
+
+export function TextField({ label, ...props }: TextFieldProps) {
   const field = useFieldContext<string>();
   const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
 
   return (
     <Field data-invalid={isInvalid}>
-      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+      <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
       <Input
         onChange={(e) => field.handleChange(e.target.value)}
         value={field.state.value}
@@ -25,7 +27,7 @@ export function TextField(
         aria-invalid={isInvalid}
         name={field.name}
         id={field.name}
-        {...textFieldProps}
+        {...props}
       />
       {isInvalid && <FieldError errors={field.state.meta.errors} />}
     </Field>
