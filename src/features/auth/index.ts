4ed3fa980b8f1be4a861b9d456ae7a3910aaaa52
Ryan Luong <lumintriet@gmail.com>
2026-06-15 07:26:42 +0700

    refactor(auth/utils): move password.ts file into utils directory.

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -11,7 +11,7 @@ import {
   MAX_PASSWORD_LENGTH,
   MIN_PASSWORD_LENGTH,
 } from "@/features/auth/constants";
-import { verifyPassword, hashPassword } from "@/features/auth/password";
+import { verifyPassword, hashPassword } from "@/features/auth/utils/password";
 import { userTable } from "@/db/schema/tables/user";
 import { serverEnv } from "@/lib/env/server";
 import { db } from "@/db";
