c1b5146fdba93bf6a0a34961060418d84b522c55
Ryan Luong <lumintriet@gmail.com>
2026-06-15 20:08:04 +0700

    feat(email/env): add smtp transporter user and password env vars.

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -4,6 +4,8 @@ const serverEnvValidator = z.object({
   SQLITE_FILE_PATH: z.string().min(1),
   BETTER_AUTH_SECRET: z.string().min(1),
   BETTER_AUTH_URL: z.url(),
+  SMTP_USER: z.string().min(1),
+  SMTP_PASS: z.string().min(1),
 });
 
 export const serverEnv = serverEnvValidator.parse(process.env);
