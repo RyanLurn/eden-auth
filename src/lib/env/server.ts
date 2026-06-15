39022e6e121fe05df9e429f9dc97ca288b2e4504
Ryan Luong <lumintriet@gmail.com>
2026-06-15 20:42:37 +0700

    feat(email/env): add more env vars for email.

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -1,9 +1,20 @@
 import z from "zod";
 
 const serverEnvValidator = z.object({
+  // For db
   SQLITE_FILE_PATH: z.string().min(1),
+  // For auth
   BETTER_AUTH_SECRET: z.string().min(1),
   BETTER_AUTH_URL: z.url(),
+  // For email
+  SMTP_HOST: z.string().min(1),
+  SMTP_PORT: z.preprocess((value) => {
+    if (typeof value === "string") {
+      return Number.parseInt(value);
+    }
+    return value;
+  }, z.int()),
+  SMTP_SECURE: z.stringbool(),
   SMTP_USER: z.string().min(1),
   SMTP_PASS: z.string().min(1),
 });
