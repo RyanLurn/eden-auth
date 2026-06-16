73eb8020cc8f3a96450f1bd71e8fe00a283fde69
Ryan Luong <lumintriet@gmail.com>
2026-06-16 08:20:42 +0700

    feat(auth/email): rework the verify email page.

--- src/routes/(auth)/verify-email/index.tsx
+++ src/routes/(auth)/verify-email/index.tsx
@@ -4,7 +4,14 @@ export const Route = createFileRoute("/(auth)/verify-email/")({
   component: VerifyEmailPage,
 });
 
-// Landing page for when we require email verification.
 function VerifyEmailPage() {
-  return <div>Please verify your email.</div>;
+  return (
+    <div className="flex size-full flex-col items-center justify-center gap-y-4">
+      <h1 className="text-2xl">Please verify your email</h1>
+      <div className="flex flex-col text-center text-muted-foreground">
+        <p>Your account has been created successfully.</p>
+        <p>Please check your inbox for a verification email.</p>
+      </div>
+    </div>
+  );
 }
