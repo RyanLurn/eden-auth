b530459f9704d71da36ca5d78bdca9f0ac656cbd
Ryan Luong <lumintriet@gmail.com>
2026-06-16 18:34:36 +0700

    feat(auth/email): handle returned error by send email verification function.

--- src/features/auth/operations/client-only/send-verification-email.ts
+++ src/features/auth/operations/client-only/send-verification-email.ts
@@ -9,10 +9,21 @@ import { authClient } from "@/features/auth/client";
 export const sendVerificationEmail = createClientOnlyFn(
   async (email: string): Promise<Result<null, UnexpectedError>> => {
     try {
-      await authClient.sendVerificationEmail({
+      const { error } = await authClient.sendVerificationEmail({
         email,
         callbackURL: CALLBACK_URL_FOR_EMAIL_VERIFICATION,
       });
+
+      // The kinds of error this operation might return is only relevant when the user is already signed in AND they want to change their email.
+      // That operation uses the same endpoint for when someone needs to verify their email on sign up. However, we don't need to prematurely generalize this method for such case right now.
+      // So, we fallback to a generic unexpected error.
+      if (error) {
+        return {
+          success: false,
+          error: new UnexpectedError({ cause: error }),
+        };
+      }
+
       return {
         success: true,
         data: null,
