79531f895f92d27584bbee217418c3f6f00d1245
Ryan Luong <lumintriet@gmail.com>
2026-06-17 09:15:54 +0700

    feat(email/types): define NodemailerError type.

--- src/features/email/types.ts
+++ src/features/email/types.ts
@@ -1,7 +1,17 @@
+import type { NODEMAILER_ERROR_CODES } from "@/features/email/error/constants";
+
 export interface Email {
   from: string;
   to: string;
   subject: string;
   text: string;
   html: string;
+}
+
+export interface NodemailerError {
+  message: string;
+  code: keyof typeof NODEMAILER_ERROR_CODES;
+  command: string;
+  response: string;
+  responseCode: number;
 }
